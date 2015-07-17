c04a9fffe363ec29810ca804ca49366ad8adad14
Dennis Heaney <dennis@learningpool.com>
2015-07-17 17:06:25 +0100

    Added some basic tracking; block states can now be saved/loaded

--- js/adapt-tincan.js
+++ js/adapt-tincan.js
@@ -9,27 +9,30 @@ define(function(require) {
   var _ = require('underscore');
   var xapi = require('extensions/adapt-tincan/js/xapiwrapper.min');
   var xapiWrapper;
+  var STATE_PROGRESS = 'adapt-course-progress';
 
   var TinCan = Backbone.Model.extend({
 
     defaults: {
-      initialised: false
+      initialised: false,
+      state: null
     },
 
     initialize: function () {
-      this.data = Adapt.config.get('_tincan');
-      if (!this.data || this.data._isEnabled === false) {
+      this.setConfig(Adapt.config.get('_tincan'));
+      if (false === this.getConfig('_isEnabled')) {
         return;
       }
 
+      this.setupListeners();
       this.xapiStart();
       $(window).unload(_.bind(this.xapiEnd, this));
-      this.onDataReady();
     },
 
     xapiStart: function () {
       // init xapi
       xapiWrapper = ADL.XAPIWrapper;
+      this.loadState();
       this.set('initialised', true);
     },
 
@@ -42,18 +45,29 @@ define(function(require) {
       xapiWrapper.sendStatement(this.getStatement(ADL.verbs.terminated));
     },
 
-    onDataReady: function () {
-      this.setupListeners();
-    },
-
     setupListeners: function () {
       Adapt.blocks.on('change:_isComplete', this.onBlockComplete, this);
       Adapt.course.on('change:_isComplete', this.onCourseComplete, this);
       Adapt.on('assessment:complete', this.onAssessmentComplete, this);
+      Adapt.on('tincan:stateChanged', this.onStateChanged, this);
+      Adapt.on('tincan:stateLoaded', this.restoreState, this);
     },
 
     onBlockComplete: function (block) {
-      console.log('block complete', block);
+      var state = this.get('state') || {};
+
+      if (!state.blocks) {
+        state.blocks = [];
+      }
+
+      state.blocks.push({
+        _id: block.get('_id'),
+        _trackingId: block.get('_trackingId'),
+        _isComplete: block.get('_isComplete'),
+      });
+
+      this.set('state', state);
+      Adapt.trigger('tincan:stateChanged');
     },
 
     onCourseComplete: function () {
@@ -65,25 +79,46 @@ define(function(require) {
     },
 
     onAssessmentComplete: function (event) {
-      Adapt.course.set('_isAssessmentPassed', event.isPass)
+      Adapt.course.set('_isAssessmentPassed', event.isPass);
+      var tracking = this.getConfig('_tracking');
 
-      // TODO persist data
+      if (!tracking) {
+        return;
+      }
 
+      // persist data
       if (event.isPass) {
-        _.defer(_.bind(this.checkTrackingCriteriaMet, this));
-      } else if (this.data._tracking._requireAssessmentPassed) { // if the user fails, don't set the status if _requireAssessmentPassed is false - doing so could overwrite the status set as a result of the user completing all the content
+        _.defer(_.bind(this.updateTrackingStatus, this));
+      } else if (tracking._requireAssessmentPassed) {
         // TODO set failed/incomplete status
       }
     },
 
+    onStateChanged: function (event) {
+      this.saveState();
+    },
+
+    /**
+    * Check if course tracking criteria have been met
+    * @return {boolean} - true, if criteria have been met; otherwise false
+    */
     checkTrackingCriteriaMet: function () {
       var criteriaMet = false;
+      var tracking = this.getConfig('_tracking');
 
-      if (this.data._tracking._requireCourseCompleted && this.data._tracking._requireAssessmentPassed) { // user must complete all blocks AND pass the assessment
-        criteriaMet = (Adapt.course.get('_isComplete') && Adapt.course.get('_isAssessmentPassed'));
-      } else if (this.data._tracking._requireCourseCompleted) { //user only needs to complete all blocks
+      if (!tracking) {
+        return criteriaMet;
+      }
+
+      if (tracking._requireCourseCompleted && tracking._requireAssessmentPassed) {
+        // user must complete all blocks AND pass the assessment
+        criteriaMet = (Adapt.course.get('_isComplete') &&
+        Adapt.course.get('_isAssessmentPassed'));
+      } else if (tracking._requireCourseCompleted) {
+        // user only needs to complete all blocks
         criteriaMet = Adapt.course.get('_isComplete');
-      } else if (this.data._tracking._requireAssessmentPassed) { // user only needs to pass the assessment
+      } else if (tracking._requireAssessmentPassed) {
+        // user only needs to pass the assessment
         criteriaMet = Adapt.course.get('_isAssessmentPassed');
       }
 
@@ -91,9 +126,9 @@ define(function(require) {
     },
 
     /**
-     * checks if course tracking criteria have been met, and sends an xAPI
-     * statement if appropriate
-     */
+    * Check if course tracking criteria have been met, and send an xAPI
+    * statement if appropriate
+    */
     updateTrackingStatus: function () {
       if (this.checkTrackingCriteriaMet()) {
         xapiWrapper.sendStatement(this.getStatement(ADL.verbs.completed));
@@ -101,12 +136,93 @@ define(function(require) {
     },
 
     /**
-     * generates a statement object for the xAPI wrapper method @sendStatement
-     *
-     * @param {string} verb - the action to register
-     * @param {string|object} [actor] - optional actor
-     * @param {object} [object] - optional object - defaults to this activity
-     */
+    * Send the current state of the course (completed blocks, duration, etc)
+    * to the LRS
+    */
+    saveState: function () {
+      if (this.get('state')) {
+        xapiWrapper.sendState(
+          this.getConfig('_activityID'),
+          this.getLRSAttribute('actor'),
+          STATE_PROGRESS,
+          null,
+          this.get('state')
+        );
+      }
+    },
+
+    /**
+    * Refresh course progress from loaded state
+    *
+    */
+    restoreState: function () {
+      var state = this.get('state');
+
+      if (!state) {
+        return;
+      }
+
+      state.blocks && _.each(state.blocks, function (block) {
+        if (block._isComplete) {
+          this.markBlockAsComplete(Adapt.blocks.findWhere({
+            _trackingId: block._trackingId
+          }));
+        }
+      }, this);
+    },
+
+    /**
+    * Loads the last saved state of the course from the LRS, if a state exists
+    *
+    * @param {boolean} async - whether to load asynchronously, default is false
+    * @fires tincan:loadStateFailed or tincan:stateLoaded
+    */
+    loadState: function (async) {
+      if (async) {
+        xapiWrapper.getState(
+          this.getConfig('_activityID'),
+          this.getLRSAttribute('actor'),
+          STATE_PROGRESS,
+          null,
+          function success (result) {
+            if ('undefined' === typeof result || 404 === result.status) {
+              Adapt.trigger('tincan:loadStateFailed');
+              return;
+            }
+
+            try {
+              this.set('state', JSON.parse(result.response));
+              Adapt.trigger('tincan:stateLoaded');
+            } catch (ex) {
+              Adapt.trigger('tincan:loadStateFailed');
+            }
+          }
+        );
+      } else {
+        this.set(
+          'state',
+          xapiWrapper.getState(
+            this.getConfig('_activityID'),
+            this.getLRSAttribute('actor'),
+            STATE_PROGRESS
+          )
+        );
+
+        if (!this.get('state')) {
+          Adapt.trigger('tincan:loadStateFailed');
+        } else {
+          Adapt.trigger('tincan:stateLoaded');
+        }
+      }
+    },
+
+    /**
+    * Generate a statement object for the xAPI wrapper method @sendStatement
+    *
+    * @param {string} verb - the action to register
+    * @param {string|object} [actor] - optional actor
+    * @param {object} [object] - optional object - defaults to this activity
+    */
     getStatement: function (verb, actor, object) {
       var statement = {
         "verb": verb
@@ -117,10 +233,57 @@ define(function(require) {
 
       // object is required, but can default to the course activity
       statement.object = object || {
-        "id": this.data._activityID
+        "id": this.getConfig('_activityID')
       }
 
       return statement;
+    },
+
+    /**
+    * Set the extension config
+    *
+    * @param {object} key - the data attribute to fetch
+    */
+    setConfig: function (conf) {
+      this.data = conf;
+    },
+
+    /**
+    * Retrieve a config item for the current course, e.g. '_activityID'
+    *
+    * @param {string} key - the data attribute to fetch
+    * @return {object|boolean} the attribute value, or false if not found
+    */
+    getConfig: function (key) {
+      if (!this.data || 'undefined' === this.data[key]) {
+        return false;
+      }
+
+      return this.data[key];
+    },
+
+    /**
+    * Retrieve an LRS attribute for the current session, e.g. 'actor'
+    *
+    * @param {string} key - the attribute to fetch
+    * @return {object|boolean} the attribute value, or false if not found
+    */
+    getLRSAttribute: function (key) {
+      if (!xapiWrapper || !xapiWrapper.lrs || 'undefined' === xapiWrapper.lrs[key]) {
+        return false;
+      }
+
+      return JSON.parse(xapiWrapper.lrs[key]);
+    },
+
+    markBlockAsComplete: function (block) {
+      if (!block || block.get('_isComplete')) {
+        return;
+      }
+
+      block.getChildren().each(function (child) {
+        child.set('_isComplete', true);
+      }, this);
     }
   });
 
