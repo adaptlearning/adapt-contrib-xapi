d91012ce83a644b9587756b8b7e1f87e9d6f3569
Barry McKay <barry@learningpool.com>
2016-01-29 11:30:47 +0000

    ADAPT-907 - Now outputting state object when it's sent to LRS. Made LRS settings more generic.

--- required/offline_xapi_wrapper.js
+++ required/offline_xapi_wrapper.js
@@ -1,26 +1,28 @@
 var xapiWrapper = {
 	lrs: {
-		actor: "{\"name\":\"User\", \"mbox\":\"mailto:noreply@learningpool.com\", \"objectType\":\"Agent\"}",
-		activityId: "http://catalogue.learningpool.com/v2/items/0123456789",
-		endpoint: "http://lrs.learningpool.com/data/xAPI/",
+		actor: "{\"objectType\": \"Agent\",\"account\": {\"homePage\": \"http://www.example.com\",\"name\": \"1625378\"}}",
+		activityId: "http://www.example.com/LA1/001/intro",
+		endpoint: "http://lrs.example.com/lrslistener/",
 	},
 
 	getState: function(activityid, agent, stateid, registration, since, callback) {
 		return null;
 	},
+
 	sendStatement: function(stmt, callback) {
-		outputStatement(stmt);
+		var a = this.lrs.actor.name;
+		var v = statement.verb.display["en-US"];
+		var o = statement.object.id;
+
+		console.log("xAPI: \"" + a + " " + v + " " + o + "\"");
+
 		return true;
 	},
+
 	sendState: function(activityid, agent, stateid, registration, stateval, matchHash, noneMatchHash, callback) {
+		console.log("xAPI: State sent to LRS");
+		console.log(stateval);
+
 		return true;
 	},
 };
-
-function outputStatement(statement) {
-	var a = this.lrs.actor.name;
-	var v = statement.verb.display["en-US"];
-	var o = statement.object.id;
-
-	console.log("xAPI: \"User " + v + " " + o + "\"");
-}
