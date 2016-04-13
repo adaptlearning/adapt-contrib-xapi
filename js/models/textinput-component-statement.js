7e0229f303cc5796cbf1e422d1671427e016a240
Ryan Lynch <rlynch77@hotmail.co.uk>
2016-04-13 16:45:53 +0100

    ADAPT-1106 - Tweaked the response so that it meets the spec. isCorrect is handled by the question component. The spec recommends the use of a scaled score so I have included this again. interactionType should be "fill-in" for a text input component. Included the [,] delimiter in the correctResponsePattern as per spec.

--- js/models/textinput-component-statement.js
+++ js/models/textinput-component-statement.js
@@ -11,32 +11,36 @@ define(function(require) {
       var result = QuestionComponentStatementModel.prototype.getResult.call(this);
       var item = this.get('model').get('_items')[0];
 
-      result.response = [];
-      var response = {};
-
-      response.userAnswer = item.userAnswer;
-      response._isCorrect = item._isCorrect;
-      result.response.push(response);
+      if (item.userAnswer != null) {
+        result.response = item.userAnswer;
+      }
 
       return result;
     },
 
     getScore: function() {
-      return QuestionComponentStatementModel.prototype.getScore.call(this);
+      var score = QuestionComponentStatementModel.prototype.getScore.call(this);
+
+      score.scaled = score.raw;
+
+      return score;
     },
 
     getObject: function() {
       var object = QuestionComponentStatementModel.prototype.getObject.call(this);
 
-      object.definition.interactionType = "textinput";
-      object.definition.correctResponsePattern = [];
+      object.definition.interactionType = "fill-in";
+
+      object.definition.correctResponsePattern = "";
+      var correctResponses = [];
 
       var item = this.get('model').get('_items')[0];
-
       _.each(item._answers, function(answer) {
-        object.definition.correctResponsePattern.push(answer);
+        correctResponses.push(answer);
 
       });
+
+      object.definition.correctResponsePattern = correctResponses.join("[,]");
 
       return object;
     },
