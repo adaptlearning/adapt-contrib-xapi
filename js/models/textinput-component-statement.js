e72cdfbfcdc27a2bbdcc3ffe50d3eedcd0331915
Ryan Lynch <rlynch77@hotmail.co.uk>
2016-04-13 15:25:29 +0100

    ADAPT-1106 - removed duplicate functions that simply need to call the parent function.

--- js/models/textinput-component-statement.js
+++ js/models/textinput-component-statement.js
@@ -7,43 +7,6 @@ define(function(require) {
 
   var TextInputComponentStatementModel = QuestionComponentStatementModel.extend({
 
-    initialize: function() {
-      return QuestionComponentStatementModel.prototype.initialize.call(this);
-    },
-
-    getStatement: function() {
-      var statement = QuestionComponentStatementModel.prototype.getStatement.call(this);
-
-      var verb = this.getVerb();
-      var object = this.getObject();
-      var context = this.getContext();
-      var result = this.getResult();
-
-      if (
-          _.isEmpty(verb) ||
-          _.isEmpty(object) ||
-          _.isEmpty(context) ||
-          _.isEmpty(result)
-      ) {
-        return null;
-      }
-
-      statement.verb = verb;
-      statement.object = object;
-      statement.context = context;
-      statement.result = result;
-
-      return statement;
-    },
-
-    getVerb: function() {
-      return QuestionComponentStatementModel.prototype.getVerb.call(this);
-    },
-
-    getObject: function() {
-      return QuestionComponentStatementModel.prototype.getObject.call(this);
-    },
-
     getResult: function() {
       var result = QuestionComponentStatementModel.prototype.getResult.call(this);
       var item = this.get('model').get('_items')[0];
