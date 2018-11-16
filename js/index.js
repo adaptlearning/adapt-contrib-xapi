11afa29fdbbf728ff198aba03a38c7dd3ac6236d
Dan <danielstorey@gmail.com>
2018-11-16 14:28:08 +0000

    Ensure course still loads on LRS error or when track state is disabled

--- js/index.js
+++ js/index.js
@@ -2,20 +2,24 @@ define([
   'core/js/adapt',
   './adapt-contrib-xapi',
   './adapt-offlineStorage-xapi'
-], function(Adapt) {
+], function(Adapt, xapi) {
 
   Adapt.on('app:dataLoaded', initialise);
 
   function initialise() {
     var config = Adapt.config.get('_xapi') || {};
-    
+
     if (!config._isEnabled) {
       return Adapt.offlineStorage.setReadyStatus();
     }
 
     Adapt.wait.begin();
 
-    Adapt.once('xapi:stateLoaded xapi:lrs:initialize:error', function() {
+    // Wait for offline storage to be restored if _shouldTrackState is enabled
+    var successEvent = config._shouldTrackState ? 'xapi:stateLoaded' : 'xapi:lrs:initialize:success';
+
+    // Ensure that the course still loads if there is a connection error
+    Adapt.once('xapi:lrs:initialize:error ' + successEvent, function() {
       Adapt.offlineStorage.get();
       Adapt.offlineStorage.setReadyStatus();
       Adapt.wait.end();
