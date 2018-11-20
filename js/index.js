b1761d6977954415706baa073c44507248d1de95
danielstorey <danielstorey@gmail.com>
2018-11-20 13:01:48 +0000

    Issue/6 (#44)
    * Set offline storage to ready if LRS connection fails
    
    This ensures that Adapt does not get stuck on the loading screen if xapi
    fails to establish a connection
    
    * Ensure course still loads on LRS error or when track state is disabled
    
    * Prevent LRS error notification disappearing when page loads
    
    * Remove unused module dep variable
    
    * Formatting
    
    * Fix useTemporaryStore check
    
    * Formatting

--- js/index.js
+++ js/index.js
@@ -8,14 +8,18 @@ define([
 
   function initialise() {
     var config = Adapt.config.get('_xapi') || {};
-    
+
     if (!config._isEnabled) {
       return Adapt.offlineStorage.setReadyStatus();
     }
 
     Adapt.wait.begin();
 
-    Adapt.once('xapi:stateLoaded', function() {
+    // Wait for offline storage to be restored if _shouldTrackState is enabled
+    var successEvent = config._shouldTrackState ? 'xapi:stateLoaded' : 'xapi:lrs:initialize:success';
+
+    // Ensure that the course still loads if there is a connection error
+    Adapt.once('xapi:lrs:initialize:error ' + successEvent, function() {
       Adapt.offlineStorage.get();
       Adapt.offlineStorage.setReadyStatus();
       Adapt.wait.end();
