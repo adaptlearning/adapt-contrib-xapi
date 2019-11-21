de3ba8d08aa07bd77be4b26c02f2cf362b468f84
Brian Quinn <brian@learningpool.com>
2019-11-21 12:51:07 +0000

    Resolves #50 and #68
    Adds support for parent and grouping contextActivities.
    
    Also uses 'body' attribute rather than 'instruction' as the question description.
    
    Bumped to a later version of xapiwrapper.min.js.

--- js/adapt-contrib-xapi.js
+++ js/adapt-contrib-xapi.js
@@ -584,7 +584,7 @@ define([
       var statement;
       var description = {};
 
-      description[this.get('displayLang')] = view.model.get('instruction');
+      description[this.get('displayLang')] = this.stripHtml(view.model.get('body'));
 
       object.definition = {
         name: this.getNameObject(view.model),
@@ -628,7 +628,20 @@ define([
       // Answered
       statement = this.getStatement(this.getVerb(ADL.verbs.answered), object, result);
 
+      this.addGroupingActivity(model, statement)
       this.sendStatement(statement);
+    },
+
+    /**
+     * Removes the HTML tags/attributes and returns a string.
+     * @param {string} html - A string containing HTML
+     * @returns {string} The same string minus HTML
+     */
+    stripHtml: function(html) {
+      var tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+
+      return tempDiv.textContent || tempDiv.innerText || '';
     },
 
     /**
@@ -665,7 +678,6 @@ define([
      * @param {AdaptModel} model - An instance of AdaptModel, i.e. ContentObjectModel, etc.
      */
     onItemExperience: function(model) {
-
       if (model.get('_id') === 'course') {
         // We don't really want to track actions on the home menu.
         return;
@@ -682,6 +694,7 @@ define([
       // Experienced.
       statement = this.getStatement(this.getVerb(ADL.verbs.experienced), object);
 
+      this.addGroupingActivity(model, statement)
       this.sendStatement(statement);
     },
 
@@ -700,7 +713,6 @@ define([
      * @param {boolean} isComplete - Flag to indicate if the model has been completed
      */
     onItemComplete: function(model, isComplete) {
-
       if (isComplete === false) {
         // The item is not actually completed, e.g. it may have been reset.
         return;
@@ -731,7 +743,42 @@ define([
       // Completed.
       statement = this.getStatement(this.getVerb(ADL.verbs.completed), object, result);
 
+      this.addGroupingActivity(model, statement)
       this.sendStatement(statement);
+    },
+
+    /**
+     * Adds a 'grouping' and/or 'parent' value to a statement's contextActivities.
+     * Note: the 'parent' is only added in the case of a question component which is part of
+     * an assessment. All articles, blocks and components are grouped by page.
+     * @param {Adapt.Model} model - Any Adapt model.
+     * @param {ADL.XAPIStatement} statement - A valid xAPI statement object.
+     */
+    addGroupingActivity: function(model, statement) {
+      var type = model.get('_type');
+
+      if (['article', 'block', 'component'].indexOf(type)) {
+        // Group these items by page/lesson.
+        var pageModel = model.findAncestor('pages')
+
+        statement.addGroupingActivity(new ADL.XAPIStatement.Activity(this.getUniqueIri(pageModel)));
+      }
+
+      if (type === 'component' && model.get('_isPartOfAssessment')) {
+        // Get the article containing this question component.
+        let articleModel = model.findAncestor('articles')
+
+        if (articleModel && articleModel.has('_assessment') && articleModel.get('_assessment')._isEnabled) {
+          // Set the assessment as the parent.
+          var assessment = {
+            _id: articleModel.get('_id'),
+            _type: 'article-assessment',
+            pageId: articleModel.get('_parentId')
+          }
+
+          statement.addParentActivity(this.getAssessmentObject(assessment))
+        }
+      }
     },
 
     /**
@@ -755,11 +802,11 @@ define([
     },
 
     /**
-     * Sends an xAPI statement when an assessment has been completed.
-     * @param {object} assessment - Object representing the state of the assessment.
+     * Gets an Activity for use in an xAPI statement.
+     * @param {object} assessment - Object representing the assessment.
+     * @returns {ADL.XAPIStatement.Activity} - Activity representing the assessment.
      */
-    onAssessmentComplete: function(assessment) {
-      var self = this;
+    getAssessmentObject: function(assessment) {
       // Instantiate a Model so it can be used to obtain an IRI.
       var fakeModel = new Backbone.Model({
         _id: assessment.articleId || assessment.id,
@@ -769,7 +816,6 @@ define([
 
       var object = new ADL.XAPIStatement.Activity(this.getUniqueIri(fakeModel));
       var name = {};
-      var statement;
 
       name[this.get('displayLang')] = assessment.id || 'Assessment';
 
@@ -778,7 +824,19 @@ define([
         type: ADL.activityTypes.assessment
       };
 
+      return object
+    },
+
+    /**
+     * Sends an xAPI statement when an assessment has been completed.
+     * @param {object} assessment - Object representing the state of the assessment.
+     */
+    onAssessmentComplete: function(assessment) {
+      var self = this;
+
+      var object = this.getAssessmentObject(assessment)
       var result = this.getAssessmentResultObject(assessment);
+      var statement;
 
       if (assessment.isPass) {
         // Passed.
