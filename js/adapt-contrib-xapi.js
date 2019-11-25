41f9d19437487649d3a4a61bedf5cf8e0b6dcc37
Brian Quinn <brian@learningpool.com>
2019-11-25 15:07:12 +0000

    Resolves #75, adds support for using registration in calls to State API (#76)
    This requires setting _shouldUseRegistration: true, the default behaviour -- consistent with the behaviour up to this point -- is to not use the registration parameter.
    
    Also added ability to surface learner info via globals.

--- js/adapt-contrib-xapi.js
+++ js/adapt-contrib-xapi.js
@@ -31,6 +31,7 @@ define([
       activityId: null,
       actor: null,
       shouldTrackState: true,
+      shouldUseRegistration: false,
       componentBlacklist: 'blank,graphic',
       isInitialised: false,
       state: {}
@@ -102,6 +103,7 @@ define([
           lang: this.getConfig('_lang'),
           generateIds: this.getConfig('_generateIds'),
           shouldTrackState: this.getConfig('_shouldTrackState'),
+          shouldUseRegistration: this.getConfig('_shouldUseRegistration') || false,
           componentBlacklist: this.getConfig('_componentBlacklist') || []
         });
 
@@ -175,6 +177,19 @@ define([
     },
 
     /**
+     * Replace the hard-coded _learnerInfo data in _globals with the actual data from the LRS.
+     */
+    getLearnerInfo: function() {
+      var globals = Adapt.course.get('_globals');
+
+      if (!globals._learnerInfo) {
+          globals._learnerInfo = {};
+      }
+
+      _.extend(globals._learnerInfo, Adapt.offlineStorage.get('learnerinfo'));
+    },
+  
+    /**
      * Intializes the ADL xapiWrapper code.
      * @param {ErrorOnlyCallback} callback
      */
@@ -184,7 +199,7 @@ define([
         //check to see if configuration has been passed in URL
         this.xapiWrapper = window.xapiWrapper || ADL.XAPIWrapper;
         if (this.checkWrapperConfig()) {
-          //URL had all necessary configuration so we continue using it
+          // URL had all necessary configuration so we continue using it.
           // Set the LRS specific properties.
           this.set({
             registration: this.getLRSAttribute('registration'),
@@ -420,6 +435,9 @@ define([
         Adapt.log.warn('adapt-contrib-xapi: Unable to setup listeners for xAPI');
         return;
       }
+
+      // Allow surfacing the learner's info in _globals.
+      this.getLearnerInfo()
 
       this.listenTo(Adapt, 'app:languageChanged', this.onLanguageChanged);
 
@@ -1071,6 +1089,9 @@ define([
       var actor = this.get('actor');
       var type = model.get('_type');
       var state = this.get('state');
+      var registration = this.get('shouldUseRegistration') === true 
+        ? this.get('registration')
+        : null;
       var collectionName = _.findKey(this.coreObjects, function(o) {
         return o === type || o.indexOf(type) > -1
       });
@@ -1098,7 +1119,7 @@ define([
       });
 
       // Pass the new state to the LRS.
-      this.xapiWrapper.sendState(activityId, actor, collectionName, null, newState, null, null, function(error, xhr) {
+      this.xapiWrapper.sendState(activityId, actor, collectionName, registration, newState, null, null, function(error, xhr) {
         if (error) {
           Adapt.trigger('xapi:lrs:sendState:error', error);
         }
@@ -1117,10 +1138,13 @@ define([
       var self = this;
       var activityId = this.get('activityId');
       var actor = this.get('actor');
+      var registration = this.get('shouldUseRegistration') === true 
+        ? this.get('registration')
+        : null;
       var state = {};
 
       Async.each(_.keys(this.coreObjects), function(type, nextType) {
-        self.xapiWrapper.getState(activityId, actor, type, null, null, function(error, xhr) {
+        self.xapiWrapper.getState(activityId, actor, type, registration, null, function(error, xhr) {
           _.defer(function() {
             if (error) {
               Adapt.log.warn('adapt-contrib-xapi: getState() failed for ' + activityId + ' (' + type + ')');
@@ -1192,9 +1216,12 @@ define([
       var self = this;
       var activityId = this.get('activityId');
       var actor = this.get('actor');
-
+      var registration = this.get('shouldUseRegistration') === true 
+        ? this.get('registration')
+        : null;
+ 
       Async.each(_.keys(this.coreObjects), function(type, nextType) {
-        self.xapiWrapper.deleteState(activityId, actor, type, null, null, null, function(error, xhr) {
+        self.xapiWrapper.deleteState(activityId, actor, type, registration, null, null, function(error, xhr) {
           if (error) {
             Adapt.log.warn('adapt-contrib-xapi: deleteState() failed for ' + activityId + ' (' + type + ')');
             return nextType(error);
