a1203cceffbff8a9b42acbf9d8526d9f5900b98c
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-08-26 14:39:32 +0100

    native functions

--- js/adapt-contrib-xapi.js
+++ js/adapt-contrib-xapi.js
@@ -185,7 +185,7 @@ class XAPI extends Backbone.Model {
       globals._learnerInfo = {};
     }
 
-    _.extend(globals._learnerInfo, Adapt.offlineStorage.get('learnerinfo'));
+    Object.assign(globals._learnerInfo, Adapt.offlineStorage.get('learnerinfo'));
   }
 
   /**
@@ -431,7 +431,7 @@ class XAPI extends Backbone.Model {
     }
 
     // Use the config to specify the core events.
-    this.coreEvents = _.extend(this.coreEvents, this.getConfig('_coreEvents'));
+    this.coreEvents = Object.assign(this.coreEvents, this.getConfig('_coreEvents'));
 
     // Always listen out for course completion.
     this.listenTo(Adapt, 'tracking:complete', this.onTrackingComplete);
@@ -459,7 +459,7 @@ class XAPI extends Backbone.Model {
 
     // Standard completion events for the various collection types, i.e.
     // course, contentobjects, articles, blocks and components.
-    _.keys(this.coreEvents).forEach(key => {
+    Object.keys(this.coreEvents).forEach(key => {
       if (key !== 'Adapt') {
         const val = this.coreEvents[key];
 
@@ -604,10 +604,10 @@ class XAPI extends Backbone.Model {
 
     if (typeof view.getInteractionObject === 'function') {
       // Get any extra interactions.
-      _.extend(object.definition, view.getInteractionObject());
+      Object.assign(object.definition, view.getInteractionObject());
 
       // Ensure any 'description' properties are objects with the language map.
-      _.keys(object.definition).forEach(key => {
+      Object.keys(object.definition).forEach(key => {
         if (object.definition[key]?.length !== 0) {
           for (let i = 0; i < object.definition[key].length; i++) {
             if (!Object.prototype.hasOwnProperty.call(object.definition[key][i], 'description')) {
@@ -1107,7 +1107,7 @@ class XAPI extends Backbone.Model {
       : null;
     const state = {};
 
-    Async.each(_.keys(this.coreObjects), (type, nextType) => {
+    Async.each(Object.keys(this.coreObjects), (type, nextType) => {
       this.xapiWrapper.getState(activityId, actor, type, registration, null, function(error, xhr) {
         _.defer(() => {
           if (error) {
@@ -1183,7 +1183,7 @@ class XAPI extends Backbone.Model {
       ? this.get('registration')
       : null;
 
-    Async.each(_.keys(this.coreObjects), (type, nextType) => {
+    Async.each(Object.keys(this.coreObjects), (type, nextType) => {
       this.xapiWrapper.deleteState(activityId, actor, type, registration, null, null, function(error, xhr) {
         if (error) {
           Adapt.log.warn('adapt-contrib-xapi: deleteState() failed for ' + activityId + ' (' + type + ')');
